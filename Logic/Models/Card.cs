77949da209df780cfcc85c99dce3c61c081aa500
arodriguezphi-cloud <arodriguezphi@gmail.com>
2026-08-17 18:34:54 -0400

    Added card class

---
+++ Logic/Models/Card.cs
@@ -0,0 +1,37 @@
+namespace Logic.Models; 
+
+public class Card
+{
+    public string Id { get; set; }
+    public Subcard[] Subcards { get; set; }
+}
+
+public class Subcard
+{
+    public string Name { get; set; }
+    public Color[] Colors { get; set; }
+    public int PlayCost { get; set; }
+    /// <summary>
+    /// Can be played if any play condition is satisfied.
+    /// </summary>
+    public PlayCondition[] PlayConditions { get; set; }
+    public string[] Traits { get; set; }
+    public Effect[] Effects { get; set; }
+}
+
+public class DigimonCard : Subcard
+{
+    public int DPower { get; set; }
+    public int Level { get; set; }
+    public Digivolution[] Digivolutions { get; set; }
+}
+
+public class OptionCard : Subcard
+{
+
+}
+
+public class TamerCard : Subcard
+{
+    
+}
